fdedc8c6af2254ae846913e434f6516e66b9d1a7
oreqizer <oreqizer@gmail.com>
2018-04-19 11:08:19 +0200

    general: Add coverage, update deps

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
   setupFiles: ["raf/polyfill", "./etc/jestSetup.js", "./etc/jestGlobals.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
   testPathIgnorePatterns: ["dist"],
+  collectCoverageFrom: ["src/client/**.{js,jsx}", "!src/client/app.jsx", "!src/client/index.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
