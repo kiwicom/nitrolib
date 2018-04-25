696ef8593634c43ffdd9bf221abfc312f82be600
oreqizer <oreqizer@gmail.com>
2018-04-25 10:56:59 +0200

    general: Add static copying

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ const gulp = require("gulp");
 const fetchBrandConfig = require("./etc/gulp/fetchBrandConfig");
 const fetchSpreadsheet = require("./etc/gulp/fetchSpreadsheet");
 
+const statics = () => gulp.src("src/static/**").pipe(gulp.dest("dist/static"));
+
 const fetch = gulp.parallel(fetchBrandConfig, fetchSpreadsheet);
 
+const build = gulp.parallel(statics, fetch);
+
 module.exports = {
+  statics,
   fetch,
+  default: build,
 };
