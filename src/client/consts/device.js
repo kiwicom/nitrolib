cf9a0543128cb0c75ccc0666598502661e0861b5
Kraus Milan <milan.kraus@vacuumlabs.com>
2018-04-25 15:13:21 +0200

    services: Add media queries definitions

---
+++ src/client/consts/device.js
@@ -0,0 +1,14 @@
+// @flow
+export const DEVICES = {
+  DESKTOP: "desktop",
+  TABLET: "tablet",
+  MOBILE: "mobile",
+};
+
+export const BREAKPOINTS = {
+  DESKTOP: 1251,
+  TABLET: 769,
+  BIG_MOBILE: 600,
+  MIDDLE_MOBILE: 414,
+  SMALL_MOBILE: 370,
+};
