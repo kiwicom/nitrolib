3e25debc978292605d8687b064b74fbd3eed84ab
oreqizer <oreqizer@gmail.com>
2018-04-26 12:48:27 +0200

    ClientOnly: Test

---
+++ src/client/components/ClientOnly/__tests__/index.spec.jsx
@@ -0,0 +1,39 @@
+// @flow
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import ClientOnly from "../";
+
+describe("#ClientOnly", () => {
+  test("mount", () => {
+    const wrapper = shallow(
+      <ClientOnly>
+        <div>kek</div>
+      </ClientOnly>,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("no mount", () => {
+    const wrapper = shallow(
+      <ClientOnly>
+        <div>kek</div>
+      </ClientOnly>,
+    );
+
+    wrapper.setState({ mounted: false });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("no mount loader", () => {
+    const wrapper = shallow(
+      <ClientOnly loader={<h2>loading...</h2>}>
+        <div>kek</div>
+      </ClientOnly>,
+    );
+
+    wrapper.setState({ mounted: false });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
