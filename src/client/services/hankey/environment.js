ea5a7216cfe65f5a94832f093bebdea73e6b52a4
oreqizer <oreqizer@gmail.com>
2018-04-26 11:57:24 +0200

    services: Add hankey environemnt, turn off camelcase

---
+++ src/client/services/hankey/environment.js
@@ -0,0 +1,27 @@
+// @flow
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+
+function fetchQuery(operation, variables) {
+  return fetch("https://nitro-hankey.skypicker.com/graphql", {
+    method: "POST",
+    headers: {
+      // Add authentication and other headers here
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: operation.text, // GraphQL text from input
+      variables,
+    }),
+  }).then(response => response.json());
+}
+
+// Create a network layer from the fetch function
+const network = Network.create(fetchQuery);
+const store = new Store(new RecordSource());
+
+const environment = new Environment({
+  network,
+  store,
+});
+
+export default environment;
