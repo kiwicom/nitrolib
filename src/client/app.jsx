305c3b4716afff77d0a6fbbba472a7707790a78c
oreqizer <oreqizer@gmail.com>
2018-05-02 10:13:32 +0200

    general: Add React strict mode

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,6 +1,6 @@
 // @flow
 /* eslint-disable no-underscore-dangle */
-import React from "react";
+import * as React from "react";
 import * as R from "ramda";
 import { hydrate } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -23,17 +23,20 @@ const intl: Intl = {
 
 if (app) {
   hydrate(
-    <BrowserRouter>
-      <ThemeProvider theme={window.__BRAND__.theme}>
-        <brandContext.Provider value={window.__BRAND__}>
-          <intlContext.Provider value={intl}>
-            <fetchedContext.Provider value={window.__FETCHED__}>
-              <Root />
-            </fetchedContext.Provider>
-          </intlContext.Provider>
-        </brandContext.Provider>
-      </ThemeProvider>
-    </BrowserRouter>,
+    // $FlowIssue: outdated Flow definitions
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={window.__BRAND__.theme}>
+          <brandContext.Provider value={window.__BRAND__}>
+            <intlContext.Provider value={intl}>
+              <fetchedContext.Provider value={window.__FETCHED__}>
+                <Root />
+              </fetchedContext.Provider>
+            </intlContext.Provider>
+          </brandContext.Provider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
     app,
   );
 }
