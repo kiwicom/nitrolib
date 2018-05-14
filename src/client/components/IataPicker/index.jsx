e75e35504a3f35af5f1d38700c0b27c5899fd5b2
oreqizer <oreqizer@gmail.com>
2018-05-14 16:23:37 +0200

    IataPicker: Add background

--- src/client/components/IataPicker/index.jsx
+++ src/client/components/IataPicker/index.jsx
@@ -6,8 +6,10 @@ import FaPlane from "react-icons/lib/fa/plane";
 import InputText from "client/components/InputText/index";
 import IconText from "client/components/IconText/index";
 import ClickOutside from "client/components/ClickOutside/index";
-import Text from "client/components/Text/index";
+import Text from "client/components/Text";
 import { Consumer as IntlConsumer } from "client/services/intl/context";
+import { brandDefault } from "../../records/Brand";
+import type { ThemeProps } from "../../records/Brand";
 import AirportListData from "./AirportListData";
 
 const Container = styled.div`
@@ -16,12 +18,19 @@ const Container = styled.div`
 
 const ResultWrapper = styled.div`
   position: absolute;
-  bottom: -300px;
+  top: 74px;
   left: 0;
   width: 100%;
-  height: 300px;
+  max-height: 300px;
   overflow-y: scroll;
+  z-index: 2;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.16), 0 1px 32px rgba(0, 0, 0, 0.32);
+  background: ${({ theme }): ThemeProps => theme.colors.white};
 `;
+
+ResultWrapper.defaultProps = {
+  theme: brandDefault.theme,
+};
 
 type Props = {|
   id: string,
