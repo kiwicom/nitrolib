64513aa624e98c7dd2213131f3844993885048a5
oreqizer <oreqizer@gmail.com>
2018-05-21 12:17:55 +0200

    booking/api: Write tests

--- src/client/services/booking/api.js
+++ src/client/services/booking/api.js
@@ -11,25 +11,23 @@ type Input = {
   departure: Date,
 };
 
-export function getTokenHandler(res: Response) {
-  if (!res.ok) {
-    return res.json().then(body => Promise.reject(body.msg));
-  }
-
-  return res.json().then(body => body.simple_token);
-}
-
 // eslint-disable-next-line import/prefer-default-export
-export function getToken({ bid, email, iata, departure }: Input) {
+export function getToken({ bid, email, iata, departure }: Input, call: typeof fetch = fetch) {
   const query = {
     email,
     src: iata,
     dtime: format(departure, config.apiDateFormat),
   };
 
-  return fetch(
+  return call(
     `${config.bookingApiUrl}/api/v0.1/users/get_simple_token/${bid}?${querystring.stringify(
       query,
     )}`,
-  ).then(getTokenHandler);
+  ).then((res: Response) => {
+    if (!res.ok) {
+      return res.json().then(body => Promise.reject(body.msg));
+    }
+
+    return res.json().then(body => body.simple_token);
+  });
 }
