88b5ceda9263b64cc85c24b8b20d807e854b2a10
oreqizer <oreqizer@gmail.com>
2018-06-04 11:18:42 +0200

    general: Remove 'src/static'

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,14 @@ const fetchBrandConfig = require("./etc/gulp/fetchBrandConfig");
 const fetchSpreadsheet = require("./etc/gulp/fetchSpreadsheet");
 const fetchTranslations = require("./etc/gulp/fetchTranslations");
 
-const statics = () => gulp.src("src/static/**").pipe(gulp.dest("dist/static"));
-
 const fetch = gulp.parallel(
   fetchBrandConfig,
   gulp.series(gulp.parallel(collectKeys, fetchSpreadsheet), fetchTranslations),
 );
 
-const build = gulp.parallel(statics, fetch);
+const build = gulp.parallel(fetch);
 
 module.exports = {
-  statics,
   fetch,
   default: build,
 };
