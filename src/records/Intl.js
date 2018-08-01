75ed954e638d2f33e115f6aa0035e0dc54619f24
oreqizer <oreqizer@gmail.com>
2018-08-01 11:16:56 +0200

    intl: Add raw type

--- src/records/Intl.js
+++ src/records/Intl.js
@@ -1,14 +1,19 @@
 // @flow strict
 import { langInfoDefault } from "./LangInfo";
 import type { LangInfo } from "./LangInfo";
-import type { Translate } from "../services/intl/translate";
+import type { Translations, Translate } from "../services/intl/translate";
+
+export type IntlRaw = {|
+  language: LangInfo,
+  translations: Translations,
+|};
 
 export type Intl = {|
-  language: LangInfo,
-  translations: {},
+  ...IntlRaw,
   translate: Translate,
 |};
 
+export type IntlsRaw = { [key: string]: IntlRaw };
 export type Intls = { [key: string]: Intl };
 
 // eslint-disable-next-line import/prefer-default-export
