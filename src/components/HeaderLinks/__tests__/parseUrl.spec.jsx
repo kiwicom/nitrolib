7eca717735fcfed8f60324b02e05b3f09274dd7c
Josip Sokec <josipokec@gmail.com>
2018-09-24 11:30:15 +0200

    FIX: parseUrl jest - clean up

--- src/components/HeaderLinks/__tests__/parseUrl.spec.jsx
+++ src/components/HeaderLinks/__tests__/parseUrl.spec.jsx
@@ -3,12 +3,20 @@
 // Methods
 import { parseLanguage, trackingUrl, parseUrl, parseParameters } from "../helpers/parseUrl";
 
-// Test data
-const SERVER_URL =
+// Original url - fetched from server
+const ORIGINAL_URL =
   "https://www.booking.com/index.html?lang={lang}&selected_currency={currency}&group_adults={adults}&group_children={children}&aid={aid}";
 
+// Original url - fetched from server - parsed
+const ORIGINAL_URL_PARSED =
+  "https://www.booking.com/index.html?lang=cro&selected_currency=eur&group_adults=1&group_children=0&aid=false";
+
+// Expected end result - url
+const RESULT_URL = `https://red-tracking.com?url=${ORIGINAL_URL_PARSED}&payload={"category":"Search","subCategory":"NavBar","action":"NavBar link clicked","destinations":{"logmole":true,"exponea":true,"ga":false}}`;
+
+// Default props
 const PROPS = {
-  link: SERVER_URL,
+  link: ORIGINAL_URL,
   currency: "eur",
   language: "cro",
   adultsCount: 1,
@@ -16,8 +24,9 @@ const PROPS = {
   aid: false,
 };
 
+// Default props - stringified (for trackingUrl method)
 const PREPARED_PROPS = {
-  link: SERVER_URL,
+  link: ORIGINAL_URL,
   preparedLang: "cro",
   preparedCurrency: "eur",
   preparedAdultsCount: "1",
@@ -25,44 +34,29 @@ const PREPARED_PROPS = {
   preparedAid: "false",
 };
 
-const SERVER_URL_PARSED =
-  "https://www.booking.com/index.html?lang=cro&selected_currency=eur&group_adults=1&group_children=0&aid=false";
-
-const RESULT_URL = `https://red-tracking.com?url=${SERVER_URL_PARSED}&payload={"category":"Search","subCategory":"NavBar","action":"NavBar link clicked","destinations":{"logmole":true,"exponea":true,"ga":false}}`;
-
 describe("parseUrl", () => {
-  // Test parseLanguage with ISO format: lowercase // TODO: change to actual format
   test("parseLanguage should return correct case1 format", () => {
     const parsedLanguage = parseLanguage("Cro", "case1");
-
     expect(parsedLanguage).toBe("cro");
   });
 
-  // Test parseLanguage with ISO format: uppercase // TODO: change to actual format
   test("parseLanguage should return correct case2 format", () => {
     const parsedLanguage = parseLanguage("Cro", "case2");
-
     expect(parsedLanguage).toBe("CRO");
   });
 
-  // Generate tracking url // TODO: Change to actual url
   test("trackingUrl should wrap original inside tracking url", () => {
-    const parsedUrl = trackingUrl(SERVER_URL_PARSED);
-
+    const parsedUrl = trackingUrl(ORIGINAL_URL_PARSED);
     expect(parsedUrl).toBe(RESULT_URL);
   });
 
-  // Replace all url variables with values
   test("parseParameters should return url with replaced variables", () => {
     const parsedUrl = parseParameters({ ...PREPARED_PROPS });
-
-    expect(parsedUrl).toBe(SERVER_URL_PARSED);
+    expect(parsedUrl).toBe(ORIGINAL_URL_PARSED);
   });
 
-  // Replace link variables (main function)
   test("trackingUrl should return correct url", () => {
     const parsedUrl = parseUrl({ ...PROPS });
-
     expect(parsedUrl).toBe(RESULT_URL);
   });
 });
