ef2361b5877cb4dae30d086055053980a6e2f05d
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2018-10-01 11:40:34 +0200

    links services: switch to langInfoDefault in tests

--- src/components/HeaderLinks/services/__tests__/carsLink.spec.js
+++ src/components/HeaderLinks/services/__tests__/carsLink.spec.js
@@ -1,54 +1,8 @@
 // @flow strict
 import getLink from "../carsLink";
+import { langInfoDefault } from "../../../../records/LangInfo";
 
-const language = {
-  addressPlaceholder: "71 Wall Stt",
-  api: "en",
-  canonical: "",
-  cityPlaceholder: "New York",
-  companyNamePlaceholder: "Kiwi.com",
-  companyVatPlaceholder: "10007",
-  countriesTranslations: "EN",
-  currency: "gbp",
-  dateFormat: "ddd D MMM",
-  dateFormatLong: "DD.MM.YYYY",
-  dateFormatPlain: "L",
-  dateFormatShort: "D/M",
-  decimalSeparator: ".",
-  defaultCountry: "gb",
-  dimension: "__x__ cm",
-  direction: "ltr",
-  displayName: "English",
-  distanceUnit: "__x__ km",
-  distanceUnitConversionRate: "1",
-  durationFormat: "H[h] mm[m]",
-  durationFormatShort: "m[m] ss[s]",
-  elevioLang: "",
-  email: "en",
-  firstNamePlaceholder: "Harry James",
-  flag: "gb",
-  fontSubsets: "greek,latin-ext",
-  hreflang: "en-GB",
-  id: "en",
-  idNumberPlaceholder: "1234567890",
-  iso: "en-GB",
-  jumio: "en_GB",
-  lastNamePlaceholder: "Brown",
-  latinInputs: false,
-  locations: "en",
-  moment: "en-gb",
-  name: "English",
-  nginx: "en",
-  phone: "gb",
-  phraseApp: "en-GB",
-  separateFourDigits: "1",
-  specialFont: "",
-  thousandsSeparator: ",",
-  timeFormat: "LT",
-  translations: "en",
-  weight: "__x__ kg",
-  zipCodePlaceholder: "10005",
-};
+const language = langInfoDefault;
 
 describe("#HeaderLinks/service/holidaysLink", () => {
   test("getLink", () => {
--- src/components/HeaderLinks/services/__tests__/roomsLInk.spec.js
+++ src/components/HeaderLinks/services/__tests__/roomsLInk.spec.js
@@ -1,5 +1,8 @@
 // @flow strict
 import getLink from "../roomsLink";
+import { langInfoDefault } from "../../../../records/LangInfo";
+
+const language = langInfoDefault;
 
 const searchFormData = {
   mode: "return",
@@ -30,55 +33,6 @@ const currencySE = {
   rate: 10,
   round: "2",
   uncertainFormat: false,
-};
-
-const language = {
-  addressPlaceholder: "71 Wall Stt",
-  api: "en",
-  canonical: "",
-  cityPlaceholder: "New York",
-  companyNamePlaceholder: "Kiwi.com",
-  companyVatPlaceholder: "10007",
-  countriesTranslations: "EN",
-  currency: "gbp",
-  dateFormat: "ddd D MMM",
-  dateFormatLong: "DD.MM.YYYY",
-  dateFormatPlain: "L",
-  dateFormatShort: "D/M",
-  decimalSeparator: ".",
-  defaultCountry: "gb",
-  dimension: "__x__ cm",
-  direction: "ltr",
-  displayName: "English",
-  distanceUnit: "__x__ km",
-  distanceUnitConversionRate: "1",
-  durationFormat: "H[h] mm[m]",
-  durationFormatShort: "m[m] ss[s]",
-  elevioLang: "",
-  email: "en",
-  firstNamePlaceholder: "Harry James",
-  flag: "gb",
-  fontSubsets: "greek,latin-ext",
-  hreflang: "en-GB",
-  id: "en",
-  idNumberPlaceholder: "1234567890",
-  iso: "en-GB",
-  jumio: "en_GB",
-  lastNamePlaceholder: "Brown",
-  latinInputs: false,
-  locations: "en",
-  moment: "en-gb",
-  name: "English",
-  nginx: "en",
-  phone: "gb",
-  phraseApp: "en-GB",
-  separateFourDigits: "1",
-  specialFont: "",
-  thousandsSeparator: ",",
-  timeFormat: "LT",
-  translations: "en",
-  weight: "__x__ kg",
-  zipCodePlaceholder: "10005",
 };
 
 describe("#HeaderLinks/service/roomsLink", () => {
