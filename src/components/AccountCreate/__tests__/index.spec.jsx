364c16e59f99d93c711a5ec8019486f1a0b29765
Daniel Kantor <git@daniel-kantor.com>
2018-10-08 18:37:40 +0200

    Add test for AccountCreate

---
+++ src/components/AccountCreate/__tests__/index.spec.jsx
@@ -0,0 +1,27 @@
+// @flow strict
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import AccountCreate from "..";
+
+import { intlDefault } from "../../../records/Intl";
+
+describe("#AccountCreate", () => {
+  test("render", () => {
+    const wrapper = shallow(
+      <AccountCreate
+        email=""
+        password=""
+        passwordConfirm=""
+        onEmailChange={() => {}}
+        onPasswordChange={() => {}}
+        onPasswordConfirmChange={() => {}}
+        onContinue={() => {}}
+        onTermsOfUse={() => {}}
+        onPrivacyPolicy={() => {}}
+      />,
+    );
+
+    expect(wrapper.prop("children")(intlDefault)).toMatchSnapshot();
+  });
+});
