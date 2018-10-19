4bc41f058114abf5ec156b56115de6c74753400d
Pavel Zbytovský <pavel.zbytovsky@kiwi.com>
2018-10-19 16:40:13 +0200

    add the Footer component

---
+++ stories/Footer.stories.jsx
@@ -0,0 +1,7 @@
+// @flow strict
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Footer from "../src/components/Footer";
+
+storiesOf("Footer", module).add("default", () => <Footer />);
