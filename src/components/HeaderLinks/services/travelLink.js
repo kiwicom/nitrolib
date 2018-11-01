1716c1bab0e58daacbf71dfd37ba22c58cdb4d30
oreqizer <oreqizer@gmail.com>
2018-11-01 11:26:54 +0100

    HeaderLinks: Relative travel link

--- src/components/HeaderLinks/services/travelLink.js
+++ src/components/HeaderLinks/services/travelLink.js
@@ -2,5 +2,5 @@
 import type { LangInfo } from "../../../records/LangInfo";
 
 export default function getLink(language: LangInfo) {
-  return `https://www.kiwi.com/${language.id}/searchDeep?pageName=search`;
+  return `/${language.id}/searchDeep?pageName=search`;
 }
