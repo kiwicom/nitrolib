0a8daf200bf56e0907bb2939c6280930ffa31761
Josip Sokec <josipokec@gmail.com>
2018-11-06 14:49:54 +0100

    FIX: stories props

--- stories/NavBar.stories.jsx
+++ stories/NavBar.stories.jsx
@@ -8,12 +8,25 @@ import NavBar from "../src/components/NavBar";
 import HeaderLinks from "../src/components/HeaderLinks";
 import withData from "./decorators/withData";
 
+const headerLinksProps = {
+  searchParams: {
+    language: "en",
+  },
+  urlParam: "search",
+  readyUrls: {
+    rooms: null,
+  },
+  hiddenUrls: {
+    holidays: false,
+    logitravel: true,
+  },
+};
+
 storiesOf("NavBar", module)
   .addDecorator(withData, withKnobs)
   .add("default", () => (
     <NavBar
-      headerLinks={<HeaderLinks />}
-      faq={<SmartFAQ />}
+      headerLinks={<HeaderLinks {...headerLinksProps} />}
       chat={<h1>Chat</h1>}
       subscription={<h1>Subscription</h1>}
       debug={<h1>Debug</h1>}
@@ -28,15 +41,23 @@ storiesOf("NavBar", module)
   ))
   .add("inverted", () => (
     <NavBar
-      headerLinks={
-        <HeaderLinks
-          linkFlights="https://www.kiwi.com/en/?headerLink=linkFlights"
-          linkRooms="https://www.kiwi.com/en/?headerLink=linkRooms"
-          linkCars="https://www.kiwi.com/en/?headerLink=linkCars"
-          linkHolidays="https://www.kiwi.com/en/?headerLink=linkHolidays"
-          inverted
-        />
-      }
+      headerLinks={<HeaderLinks {...headerLinksProps} />}
+      chat={<h1>Chat</h1>}
+      subscription={<h1>Subscription</h1>}
+      debug={<h1>Debug</h1>}
+      starred={<span>Starred</span>}
+      inverted
+      portal=""
+      onOpenFaq={action("Open FAQ")}
+      onLogoClick={action("Click logo")}
+      onSetModal={action("Set modal")}
+      onSaveLanguage={action("Save language")}
+      onSelectTrip={action("Select trip")}
+    />
+  ))
+  .add("inverted", () => (
+    <NavBar
+      headerLinks={<HeaderLinks {...headerLinksProps} />}
       chat={<h1>Chat</h1>}
       subscription={<h1>Subscription</h1>}
       debug={<h1>Debug</h1>}
