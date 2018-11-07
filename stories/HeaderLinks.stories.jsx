fe2a845cf29ec29c8882ed8bae42211a5cc338f6
Josip Sokec <josipokec@gmail.com>
2018-11-07 09:23:07 +0100

    ReadyUrls rooms flow - headerLinks

--- stories/HeaderLinks.stories.jsx
+++ stories/HeaderLinks.stories.jsx
@@ -14,7 +14,10 @@ const headerLinksProps = {
   },
   urlParam: "search",
   readyUrls: {
-    rooms: null,
+    rooms: {
+      query: "query",
+      base: "base",
+    },
   },
   hiddenUrls: {
     holidays: false,
