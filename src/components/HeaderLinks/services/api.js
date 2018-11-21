9730f3ee4a6a0cb508dca01b2d254ab6c3e02200
Josip Sokec <josipokec@gmail.com>
2018-11-21 14:01:14 +0100

    Api / fetch fix

--- src/components/HeaderLinks/services/api.js
+++ src/components/HeaderLinks/services/api.js
@@ -1,12 +1,9 @@
 // @flow
 
-const fetch = require("node-fetch");
+import { handleJSON } from "../../../services/fetch/handlers";
 
-export const getNavBarLinks = () =>
-  fetch("https://brxh1qilc2.execute-api.eu-central-1.amazonaws.com/staging/navbar", {
+export default function getNavBarLinks() {
+  return fetch("https://brxh1qilc2.execute-api.eu-central-1.amazonaws.com/staging/navbar", {
     method: "GET",
-  }).then(res => res.json());
-
-export default {
-  getNavBarLinks,
-};
+  }).then(handleJSON);
+}
