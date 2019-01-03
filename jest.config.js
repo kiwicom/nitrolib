89bbf5715fb7ac61ce52a2d4bb9d81ed64431a0b
oreqizer <oreqizer@gmail.com>
2019-01-03 14:47:05 +0530

    general: Turn off useless Flow

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
+// @noflow
 module.exports = {
   setupFiles: ["raf/polyfill", "./etc/jestSetup.js"],
   setupTestFrameworkScriptFile: "./etc/jestSetupFramework.js",
