d85e42a7a58b506dcc2d376f7a2abeb4fcdee9a2
Matus Stastny <matus.stastny@kiwi.com>
2019-01-08 10:45:35 +0100

    Component: Add mobile layout

--- src/components/BaggagePicker/BaggeOptions/OptionItem/index.jsx
+++ src/components/BaggagePicker/BaggeOptions/OptionItem/index.jsx
@@ -2,7 +2,11 @@
 import * as React from "react";
 import Stack from "@kiwicom/orbit-components/lib/Stack";
 import Text from "@kiwicom/orbit-components/lib/Text";
+import styled, { css } from "styled-components";
 
+import mq from "../../../../styles/mq";
+import { themeDefault } from "../../../../records/Theme";
+import type { ThemeProps } from "../../../../records/Theme";
 import type { Price, Restrictions } from "../../../../records/Baggage";
 
 type Props = {|
@@ -16,6 +20,64 @@ type Props = {|
   baggageSize: string,
 |};
 
+type BaggageSizeTextProps = ThemeProps & {
+  isMobile: boolean,
+};
+
+const BaggageSizeText = styled.p`
+  display: ${({ isMobile }: BaggageSizeTextProps) => (isMobile ? "none" : "block")};
+  color: ${({ theme }): ThemeProps => theme.orbit.colorTextSecondary};
+  font-size: ${({ theme, isMobile }: BaggageSizeTextProps) =>
+    isMobile ? theme.orbit.fontSizeTextSmall : theme.orbit.fontSizeTextNormal};
+  font-family: ${({ theme }): ThemeProps => theme.orbit.fontFamily};
+  margin: 0;
+
+  ${mq.ltBigMobile(css`
+    display: ${({ isMobile }: BaggageSizeTextProps) => (isMobile ? "block" : "none")};
+  `)};
+`;
+
+BaggageSizeText.defaultProps = {
+  theme: themeDefault,
+  isMobile: false,
+};
+
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  flex-grow: 1;
+  flex-shrink: 1;
+  justify-content: flex-start;
+  align-content: center;
+
+  ${mq.ltBigMobile(css`
+    display: flex;
+  `)};
+
+  > * {
+    margin-right: ${({ theme }): ThemeProps => theme.orbit.spaceXSmall};
+  }
+`;
+TitleWrapper.defaultProps = {
+  theme: themeDefault,
+};
+
+const Title = styled.span`
+  line-height: 24px;
+`;
+
+const BaggageInfoWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  ${mq.ltBigMobile(css`
+    justify-content: flex-end;
+    width: 50%;
+  `)};
+`;
+
 const OptionItem = ({
   firstItem,
   amount,
@@ -27,21 +89,30 @@ const OptionItem = ({
   categoryName,
 }: Props) => (
   <Stack shrink spacing="natural" align="center">
-    <Stack shrink align="center" spacing="condensed">
+    <Stack shrink spacing="condensed">
       {categoryIcon}
-      <Text>
-        {amount > 1 && (
-          <Text element="span" weight="bold">
-            {`${amount}x `}
-          </Text>
-        )}
-        {holdBag && `${restrictions.weight}kg`} {categoryName}
-      </Text>
+      <TitleWrapper>
+        <Text>
+          <Title>
+            {amount > 1 && (
+              <Text element="span" weight="bold">
+                {`${amount}x `}
+              </Text>
+            )}
+            {holdBag && `${restrictions.weight}kg`} {categoryName}
+          </Title>
+        </Text>
+        <BaggageSizeText isMobile>{baggageSize}</BaggageSizeText>
+      </TitleWrapper>
     </Stack>
-    <Stack shrink align="center" justify="between" spacing="tight">
-      <Text type="secondary">{baggageSize}</Text>
-      {firstItem ? <Text weight="bold">{`${price.amount} ${price.currency}`}</Text> : <span />}
-    </Stack>
+    <BaggageInfoWrapper>
+      <BaggageSizeText>{baggageSize}</BaggageSizeText>
+      {firstItem ? (
+        <Text element="span" weight="bold">{`${price.amount} ${price.currency}`}</Text>
+      ) : (
+        <span />
+      )}
+    </BaggageInfoWrapper>
   </Stack>
 );
 
