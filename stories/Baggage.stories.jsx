8e56bc9679e7105316d56a26ecbf054747495440
Matus Stastny <matus.stastny@kiwi.com>
2019-01-08 12:46:49 +0100

    Baggage: Add knobs for changing selected option

--- stories/Baggage.stories.jsx
+++ stories/Baggage.stories.jsx
@@ -1,14 +1,31 @@
 // @flow strict
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, select, text, boolean, number } from "@storybook/addon-knobs";
 
 import Baggage from "../src/components/Baggage";
 import baggageData from "../src/components/Baggage/baggageData";
 
+const handBagOptions = {
+  first: 0,
+  second: 1,
+  third: 2,
+  fourth: 3,
+  fifth: 4,
+};
+
+const holdBagOptions = {
+  first: 5,
+  second: 6,
+  third: 7,
+  fourth: 8,
+  fifth: 9,
+  sixth: 10,
+};
+
 const props = {
   passengerIndex: 0,
   passengerCategory: "adult",
-  passengerBaggage: { handBag: 1, holdBag: 7 },
   changeBagCombination: (type, index) => {
     console.log(type, index);
   },
@@ -21,8 +38,16 @@ const props = {
   airlines: [],
 };
 
-storiesOf("Baggage", module).add("default", () => (
-  <div style={{ padding: "24px" }}>
-    <Baggage {...props} />
-  </div>
-));
+storiesOf("Baggage", module)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <div style={{ padding: "24px" }}>
+      <Baggage
+        passengerBaggage={{
+          handBag: select("Handbag", handBagOptions, 1, "Baggage"),
+          holdBag: select("Holdbag", holdBagOptions, 6, "Baggage"),
+        }}
+        {...props}
+      />
+    </div>
+  ));
