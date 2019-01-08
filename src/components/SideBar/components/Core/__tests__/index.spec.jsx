91c49dd481ee74d1b360f9776c2cd29ebc2d70ae
Alex Alexeev <alex.alexeev@kiwi.com>
2019-01-08 14:54:28 +0100

    SideBar: Test unmasked prop

--- src/components/SideBar/components/Core/__tests__/index.spec.jsx
+++ src/components/SideBar/components/Core/__tests__/index.spec.jsx
@@ -57,6 +57,26 @@ describe("#index", () => {
     expect(wrappr).toHaveStyleRule("transform", "translate3d(0)");
   });
 
+  test("render entered unmasked", () => {
+    const onClick = jest.fn();
+    const wrapper = mount(
+      <Core onClick={onClick} status="entered" unmasked>
+        <h1>Kek</h1>
+      </Core>,
+    );
+
+    const container = wrapper.find("Core__Container");
+    const wrappr = wrapper.find("Core__Wrapper");
+
+    expect(container).toHaveStyleRule("visibility", "visible");
+    expect(container).toHaveStyleRule("background-color", "transparent");
+    expect(wrappr).toHaveStyleRule("right", "0");
+    expect(wrappr).toHaveStyleRule("transform", "translate3d(0)");
+    wrapper.find("Core__Container").simulate("click");
+
+    expect(onClick).not.toBeCalled();
+  });
+
   test("render exiting inverted", () => {
     const wrapper = mount(
       <Core onClick={jest.fn()} status="exiting" inverted>
