2cdec774ec33ef01111121f941b5f135064d2ec7
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-01-09 15:56:34 -0300

    AccountCreate: Add unit tests covering validation errors

--- src/components/AccountCreate/__tests__/index.spec.jsx
+++ src/components/AccountCreate/__tests__/index.spec.jsx
@@ -22,4 +22,23 @@ describe("#AccountCreate", () => {
 
     expect(wrapper.prop("children")(intlDefault)).toMatchSnapshot();
   });
+
+  it('shows validation errors', () => {
+    const wrapper = shallow(
+      <AccountCreate
+        email=""
+        password=""
+        passwordConfirm=""
+        onEmailChange={() => {}}
+        onPasswordChange={() => {}}
+        onPasswordConfirmChange={() => {}}
+        onContinue={() => {}}
+        emailError="Wrong format of e-mail"
+        passwordError="Password should be more complex"
+        passwordConfirmError="Passwords do not match"
+      />,
+    );
+
+    expect(wrapper.prop("children")(intlDefault)).toMatchSnapshot();
+  });
 });
