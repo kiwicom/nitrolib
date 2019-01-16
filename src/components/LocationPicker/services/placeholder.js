3df637df87e5811fe3ea5d3bb55d5828838c7c5a
oreqizer <oreqizer@gmail.com>
2019-01-16 20:20:52 +0530

    LocationPicker: CR

--- src/components/LocationPicker/services/placeholder.js
+++ src/components/LocationPicker/services/placeholder.js
@@ -3,21 +3,15 @@ import type { Location } from "../../../records/Location";
 
 const placeholder = (loc: Location): string => {
   const { type, name } = loc;
-  const countryName = loc.country ? `(${loc.country.name})` : "";
-  const codeSlug = loc.code ? `(${loc.code})` : "";
 
   switch (type) {
     case "city":
-      return `${name} ${countryName}`;
-
     case "autonomous_territory":
-      return `${name} ${countryName}`;
+      return `${name} ${(loc.country ? `(${loc.country.name})` : "")}`;
 
     case "airport":
-      return `${name} ${codeSlug}`;
-
     case "station":
-      return `${name} ${codeSlug}`;
+      return `${name} ${(loc.code ? `(${loc.code})` : "")}`;
 
     default:
       return name;
