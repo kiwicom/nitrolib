fafc75df2db9152313c7007babd659e3da15bca0
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-01-17 19:04:43 -0300

    AccountLogin: Add "type" prop to show multiple reasons why to log in

--- src/components/AccountLogin/__tests__/index.spec.jsx
+++ src/components/AccountLogin/__tests__/index.spec.jsx
@@ -6,18 +6,18 @@ import AccountLogin from "..";
 
 import { intlDefault } from "../../../records/Intl";
 
+const commonProps = {
+  email: "",
+  onNoAccount: () => {},
+  onEmailChange: () => {},
+  onFacebookLogin: () => {},
+  onGoogleLogin: () => {},
+  onContinue: () => {},
+};
+
 describe("#AccountLogin", () => {
   test("render", () => {
-    const wrapper = shallow(
-      <AccountLogin
-        email=""
-        onNoAccount={() => {}}
-        onEmailChange={() => {}}
-        onFacebookLogin={() => {}}
-        onGoogleLogin={() => {}}
-        onContinue={() => {}}
-      />,
-    );
+    const wrapper = shallow(<AccountLogin {...commonProps} />);
 
     // <span /> needed for some reason, but it works
     const wrapper2 = shallow(<span>{wrapper.prop("children")(intlDefault)}</span>);
@@ -31,46 +31,38 @@ describe("#AccountLogin", () => {
     ).toBe("account.manage_your_bookings");
   });
 
-  test("render custom illustration", () => {
-    const wrapper = shallow(
-      <AccountLogin
-        email=""
-        illustration="Help"
-        onNoAccount={() => {}}
-        onEmailChange={() => {}}
-        onFacebookLogin={() => {}}
-        onGoogleLogin={() => {}}
-        onContinue={() => {}}
-      />,
-    );
+  [
+    {
+      type: "mmb",
+      description: "account.sign_in_description",
+      illustration: "Login",
+      title: "account.manage_your_bookings",
+    },
+    {
+      type: "help",
+      description: "account.login_description.help",
+      illustration: "Help",
+      title: "account.login_title.get_help",
+    },
+    {
+      type: "refer",
+      description: "account.login_description.refer",
+      illustration: "ReferAFriend",
+      title: "account.login_title.refer",
+    },
+  ].forEach(({ type, description, illustration, title }) =>
+    test(`type ${type}`, () => {
+      const wrapper = shallow(<AccountLogin {...commonProps} type={type} />);
 
-    // <span /> needed for some reason, but it works
-    const wrapper2 = shallow(<span>{wrapper.prop("children")(intlDefault)}</span>);
-
-    expect(wrapper2.find("Illustration").prop("name")).toBe("Help");
-  });
-
-  test("render custom text", () => {
-    const wrapper = shallow(
-      <AccountLogin
-        email=""
-        text={<span className="Text">Kek</span>}
-        onNoAccount={() => {}}
-        onEmailChange={() => {}}
-        onFacebookLogin={() => {}}
-        onGoogleLogin={() => {}}
-        onContinue={() => {}}
-      />,
-    );
-
-    // <span /> needed for some reason, but it works
-    const wrapper2 = shallow(<span>{wrapper.prop("children")(intlDefault)}</span>);
-
-    expect(
-      wrapper2
-        .find("Heading")
-        .children(".Text")
-        .text(),
-    ).toBe("Kek");
-  });
+      const component = shallow(<span>{wrapper.prop("children")(intlDefault)}</span>);
+      expect(component.find("Illustration").prop("name")).toBe(illustration);
+      expect(
+        component
+          .find("Heading")
+          .find("Translate")
+          .prop("t"),
+      ).toBe(title);
+      expect(component.find(`[t="${description}"]`).exists()).toBe(true);
+    }),
+  );
 });
