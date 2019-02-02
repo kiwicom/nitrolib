f3792326d299cd84b7faccaedc869da6aa36ff4c
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-02-03 00:19:57 +0100

    MagicLogin: Reorganize main failes to export types

---
+++ src/components/MagicLogin/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export { default } from "./MagicLogin";
+
+export type { LoginType, Screen } from "./types";
+export type { SignInUser } from "./mutations/__generated__/SignInUser.graphql";
