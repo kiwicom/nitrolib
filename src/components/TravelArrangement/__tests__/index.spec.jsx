1eabfe6207b64484dd5bebcc939067dc5143d201
Andrea Sikimic <andrea.sikimic@kiwi.com>
2019-02-12 15:28:50 +0100

    TravelArrangement: Added test

--- src/components/TravelArrangement/__tests__/index.spec.jsx
+++ src/components/TravelArrangement/__tests__/index.spec.jsx
@@ -5,7 +5,7 @@ import { shallow } from "enzyme";
 import TravelArrangement from "..";
 
 describe("#TravelArrangement", () => {
-  test("render", () => {
+  test("render - row", () => {
     const data = {
       takeOff: {
         from: "Prague, Czech Republic",
@@ -21,6 +21,26 @@ describe("#TravelArrangement", () => {
       },
     };
     const wrapper = shallow(<TravelArrangement data={data} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("render - column", () => {
+    const data = {
+      takeOff: {
+        from: "Prague, Czech Republic",
+        to: "Denpasar, Bali",
+        stops: "0",
+        note: "Transfer to hotel NOT included",
+      },
+      landing: {
+        from: "Denpasar, Bali",
+        to: "Prague, Czech Republic",
+        stops: "2",
+        note: "Transfer to hotel NOT included",
+      },
+    };
+    const wrapper = shallow(<TravelArrangement data={data} columnLayout />);
 
     expect(wrapper).toMatchSnapshot();
   });
