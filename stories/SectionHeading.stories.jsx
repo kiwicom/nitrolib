2c13acf81c28e465d8a2a0046a3a29cf2aab20ea
Andrea Sikimic <andrea.sikimic@kiwi.com>
2019-02-13 11:48:20 +0100

    stories: Reverting change

--- stories/SectionHeading.stories.jsx
+++ stories/SectionHeading.stories.jsx
@@ -1,11 +1,11 @@
-// // @flow strict
-// import * as React from "react";
-// import { storiesOf } from "@storybook/react";
-// import Airplane from "@kiwicom/orbit-components/lib/icons/Airplane";
+// @flow strict
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import Airplane from "@kiwicom/orbit-components/lib/icons/Airplane";
 
-// import SectionHeading from "../src/components/SectionHeading";
-// import withData from "./decorators/withData";
+import SectionHeading from "../src/components/SectionHeading";
+import withData from "./decorators/withData";
 
-// storiesOf("SectionHeading", module)
-//   .addDecorator(withData)
-//   .add("default", () => <SectionHeading t="common.ok" icon={<Airplane />} />);
+storiesOf("SectionHeading", module)
+  .addDecorator(withData)
+  .add("default", () => <SectionHeading t="common.ok" icon={<Airplane />} />);
