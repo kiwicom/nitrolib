dedcc4bf36dcafa7b62337b2bd93e8d349ad002c
Andrea Sikimic <andrea.sikimic@kiwi.com>
2019-02-15 11:48:41 +0100

    travelArrangement: Fixed layout to be responsive

--- src/components/TravelArrangement/index.jsx
+++ src/components/TravelArrangement/index.jsx
@@ -1,45 +1,63 @@
 // @flow
 
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Stack from "@kiwicom/orbit-components/lib/Stack";
 import Heading from "@kiwicom/orbit-components/lib/Heading";
+import mq from "@kiwicom/orbit-components/lib/utils/mediaQuery";
 
 import Translate from "../Translate";
 import Sector from "./components/Sector";
+import { themeDefault } from "../../records/Theme";
 import type { TravelArrangement as TravelArrangementType } from "../../records/TravelArrangement";
+import type { ThemeProps } from "../../records/Theme";
 
 type Props = {|
   data: TravelArrangementType,
-  columnLayout?: boolean,
 |};
 
 const SectorWrapper = styled.div`
   flex-grow: 1;
   flex-basis: 0;
+  :first-child {
+    margin-bottom: ${({ theme }: ThemeProps) => theme.orbit.spaceLarge};
+  }
+
+  ${mq.largeMobile(css`
+    :first-child {
+      margin-right: ${({ theme }: ThemeProps) => theme.orbit.spaceLarge};
+      margin-bottom: 0;
+    }
+  `)};
+`;
+SectorWrapper.defaultProps = {
+  theme: themeDefault,
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${mq.largeMobile(css`
+    flex-direction: row;
+  `)};
 `;
 
-const TravelArrangement = ({ columnLayout, data }: Props) => {
+const TravelArrangement = ({ data }: Props) => {
   const { takeOff, landing } = data;
-  const content = [
-    <SectorWrapper key="1">
-      <Sector data={takeOff} direction="takeOff" />
-    </SectorWrapper>,
-    <SectorWrapper key="2">
-      <Sector data={landing} direction="landing" />
-    </SectorWrapper>,
-  ];
 
   return (
     <Stack spaceAfter="large">
       <Heading element="h4" type="title3" spaceAfter="small">
         <Translate t="holidays.detail.travel_arrangement" />
       </Heading>
-      {columnLayout ? (
-        content
-      ) : (
-        <Stack desktop={{ direction: "row", spacing: "comfy" }}>{content}</Stack>
-      )}
+      <Wrapper>
+        <SectorWrapper key="1">
+          <Sector data={takeOff} direction="takeOff" />
+        </SectorWrapper>
+        <SectorWrapper key="2">
+          <Sector data={landing} direction="landing" />
+        </SectorWrapper>
+      </Wrapper>
     </Stack>
   );
 };
