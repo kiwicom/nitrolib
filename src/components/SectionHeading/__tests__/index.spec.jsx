a409a2a318d9ac9b6037a1eb66764fa38272746a
Andrea Sikimic <andrea.sikimic@kiwi.com>
2019-02-18 11:14:53 +0100

    tests: Removing snapshots

--- src/components/SectionHeading/__tests__/index.spec.jsx
+++ src/components/SectionHeading/__tests__/index.spec.jsx
@@ -1,14 +1,14 @@
-// @flow
+// @flow strict
 import * as React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import Airplane from "@kiwicom/orbit-components/lib/icons/Airplane";
 
 import SectionHeading from "..";
 
 describe("#SectionHeading", () => {
   test("render", () => {
-    const wrapper = shallow(<SectionHeading t="test" icon={<Airplane />} />);
+    const wrapper = mount(<SectionHeading t="test" icon={<Airplane />} />);
 
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.contains("test")).toBe(true);
   });
 });
