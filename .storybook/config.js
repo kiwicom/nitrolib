ff024050d8ab2bb510dd2c067fd79016d4f349fc
mainframev <vgenaev@gmail.com>
2019-03-06 12:00:33 +0100

    general: Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from "@storybook/addon-info";
+import { create } from '@storybook/theming';
+
 
 // automatically import all files ending in *.stories.jsx
+const theme = create({
+  base: 'light',
+  colorPrimary: '#00a991',
+  colorSecondary: '#bac7d5',
+  fontBase: '"Roboto", -apple-system, ".SFNSText-Regular", "San Francisco", "Segoe UI", "Helvetica Neue", "Lucida Grande", sans-serif',
+});
+
+addParameters({ options: { theme } });
+
 const req = require.context('../stories', true, /.stories.jsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
