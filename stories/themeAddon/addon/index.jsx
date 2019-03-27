646533755e9d50bd724c5cb82e3a1f80cf1e8ffd
mainframev <vgenaev@gmail.com>
2019-03-27 23:32:00 +0100

    Storybook: Moved themes to panel

--- stories/themeAddon/addon/index.jsx
+++ stories/themeAddon/addon/index.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 import * as React from "react";
 import addons, { types } from "@storybook/addons";
-import { TabButton, WithTooltip, TooltipLinkList } from "@storybook/components";
+import { TooltipLinkList } from "@storybook/components";
 import { FORCE_RE_RENDER } from "@storybook/core-events";
 
 import themes from "../allThemes";
@@ -12,10 +12,6 @@ const STORAGE = "theme";
 
 type Props = {|
   api: any,
-|};
-type State = {|
-  active: boolean,
-  theme: ThemeNames,
 |};
 
 type Args = {|
@@ -30,73 +26,33 @@ type Link = {|
   onClick: () => void,
 |};
 
-class ThemeSwitcher extends React.Component<Props, State> {
-  state = { theme: "default", active: false };
+const setTheme = ({ api, set, rerender }: Args) => {
+  window.localStorage.setItem(STORAGE, set);
 
-  componentDidMount() {
-    const stored = window.localStorage.getItem(STORAGE);
+  api.setOptions({
+    theme: themes[set],
+  });
 
-    this.setState({
-      theme: window.localStorage.getItem(STORAGE) !== null ? stored : "default",
-    });
+  if (rerender) {
+    addons.getChannel().emit(FORCE_RE_RENDER);
   }
+};
 
-  handleActive = () => {
-    this.setState({
-      // eslint-disable-next-line
-      active: !this.state.active,
-    });
-  };
+const ThemeSwitcher = ({ api }: Props) => {
+  const links: Array<Link> = Object.keys(themes).map(i => ({
+    id: i,
+    title: i,
+    onClick: () => {
+      setTheme({ api, set: i, rerender: true });
+    },
+  }));
 
-  handleTheme = (theme: ThemeNames) => {
-    this.setState({
-      theme,
-    });
-  };
-
-  setTheme = ({ api, set, rerender }: Args) => {
-    window.localStorage.setItem(STORAGE, set);
-
-    api.setOptions({
-      theme: themes[set],
-    });
-
-    if (rerender) {
-      addons.getChannel().emit(FORCE_RE_RENDER);
-    }
-  };
-
-  render() {
-    const { theme, active } = this.state;
-    const { api } = this.props;
-
-    const links: Array<Link> = Object.keys(themes).map(i => ({
-      id: i,
-      title: i,
-      onClick: () => {
-        this.handleTheme(i);
-        this.setTheme({ api, set: i, rerender: true });
-      },
-    }));
-
-    return (
-      <WithTooltip
-        placement="top"
-        trigger="click"
-        tooltipShown={active}
-        onVisibilityChange={this.handleActive}
-        tooltip={<TooltipLinkList links={links} />}
-        closeOnClick
-      >
-        <TabButton>{theme}</TabButton>
-      </WithTooltip>
-    );
-  }
-}
+  return <TooltipLinkList links={links} />;
+};
 
 addons.register(ADDON_ID, api => {
   addons.add(ADDON_ID, {
-    type: types.TOOL,
+    type: types.PANEL,
     title: "themes",
     match: ({ viewMode }) => viewMode === "story",
     render: () => <ThemeSwitcher api={api} />,
