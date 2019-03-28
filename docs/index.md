6cd42ff7d4008976fa52b7c99466140de4cbfb7a
Luděk Vepřek <weprous@gmail.com>
2019-03-28 06:07:25 +0100

    General: update orbit-components to 0.37.0

--- docs/index.md
+++ docs/index.md
@@ -3,7 +3,7 @@
 * `yarn add @kiwicom/nitro`
 
 **Peer dependencies:**
-* `@kiwicom/orbit-components: ^0.35.0`
+* `@kiwicom/orbit-components: ^0.37.0`
 * `ramda: ^0.26.1`
 * `react: ^16.8.0`
 * `react-dom: ^16.8.0`
--- src/records/Theme.js
+++ src/records/Theme.js
@@ -1,12 +1,12 @@
 // @flow strict
-import defaultTokens, { type Theme } from "@kiwicom/orbit-components/lib/defaultTokens";
+import defaultTheme, { type Theme } from "@kiwicom/orbit-components/lib/defaultTheme";
 import fromPlainObject from "@kiwicom/orbit-components/lib/fromPlainObject";
 
 import type { Brand } from "./Brand";
 
 export type ThemeProps = {| theme: Theme |};
 
-export const themeDefault: Theme = defaultTokens;
+export const themeDefault: Theme = defaultTheme;
 
 export const getBrandTheme = (brand: Brand, rtl: boolean = false): Theme => ({
   orbit: fromPlainObject(brand.theme.palette),
