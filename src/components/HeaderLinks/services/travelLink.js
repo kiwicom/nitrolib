b6b83675d35dc21ce7b67306a3023e6b6f4ca238
Josip Sokec <josip.sokec@kiwi.com>
2019-05-17 19:27:49 +0200

    refactor(header-links): Removed unused functions

--- src/components/HeaderLinks/services/travelLink.js
+++
@@ -1,6 +0,0 @@
-// @flow strict
-import type { LangInfo } from "../../../records/LangInfo";
-
-export default function getLink(language: LangInfo) {
-  return `/${language.id}/searchDeep?pageName=search`;
-}
