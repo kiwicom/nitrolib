8aa08a7c8358251814c804ca1230dbd657497c7b
oreqizer <oreqizer@gmail.com>
2019-06-03 09:29:37 +0200

    fix(services): `log/api` returns a promise

--- src/services/log/api.js
+++ src/services/log/api.js
@@ -3,12 +3,13 @@ import type { Loglady } from "../../records/Loglady";
 import { JSON_BOTH } from "../fetch/headers";
 import { handleError } from "../fetch/handlers";
 
-function log(payload: Loglady) {
+const log = (payload: Loglady): Promise<void> =>
   fetch("https://loglady.skypicker.com/api-docs/track", {
     method: "POST",
     headers: JSON_BOTH,
     body: JSON.stringify(payload),
-  }).then(handleError);
-}
+  })
+    .then(handleError)
+    .then(() => {});
 
 export default log;
