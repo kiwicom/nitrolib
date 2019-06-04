0730f1676ee922291ff4036ff0e300981b85a969
Josip Šokec <josipokec@gmail.com>
2019-06-04 14:48:26 +0200

    fix(headerlinks): Flow fix

--- stories/NavBar.stories.jsx
+++ stories/NavBar.stories.jsx
@@ -26,8 +26,7 @@ const props = {
   splitster: {
     HEADER_LINKS_ROOMS_PROVIDER_0: "roomsKiwiCode",
   },
-  context: "search",
-  brand: "Kiwi.com",
+  context: "Kiwi.com",
 };
 
 storiesOf("NavBar", module)
