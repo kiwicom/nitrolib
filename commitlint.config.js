b2b271645fc469259f0be3275e7bde56118d3fb6
oreqizer <oreqizer@gmail.com>
2019-06-05 11:38:03 +0200

    feat(meta): Add scope enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [2, "always", "sentence-case"],
+    "scope-enum": [2, "always", ["meta", "bin", "components", "consts", "records", "services"]],
   },
 };
