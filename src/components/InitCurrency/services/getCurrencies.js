17248f3606b50248527734151327c1327a675de9
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2019-06-12 11:51:49 +0200

    revert: InitCurrency Launchpad/finance -> hankey until finance publishes proper prod

--- src/components/InitCurrency/services/getCurrencies.js
+++ src/components/InitCurrency/services/getCurrencies.js
@@ -2,24 +2,8 @@
 import type { FetchedCurrencies } from "../../../records/Currency";
 
 const getCurrencies = (): Promise<FetchedCurrencies> =>
-  Promise.all([
-    fetch("https://finance-launchpad.skypicker.com/search")
-      .then(res => res.json())
-      .then(res => res.currencies.supported_currencies),
-    fetch("https://rates-finance.skypicker.com/").then(res => res.json()),
-  ]).then(([list, rates]) =>
-    list
-      .map(entry => ({
-        id: entry.code.toLowerCase(),
-        name: entry.name,
-        precision: entry.precision,
-        fallback: "eur",
-        enabledOnAffilId: entry.affil_id.length > 1 ? entry.affil_id : "",
-        format: entry.format,
-        uncertainFormat: false,
-        rate: 1 / rates[entry.code.toUpperCase()].value,
-      }))
-      .reduce((acc, next) => ({ ...acc, [next.id]: next }), {}),
+  fetch("https://nitro-hankey.skypicker.com/currencies").then(res =>
+    res.json()
   );
 
 export default getCurrencies;
