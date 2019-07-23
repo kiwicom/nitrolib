2657e737458e13b3926d37252aef320573d6ab03
Matus Stastny <matus.stastny@kiwi.com>
2019-07-23 14:11:04 +0200

    fix(components): Tests in PriorityBoardingInfo

--- src/components/BaggagePicker/components/Option/components/PriorityBoardingInfo/__tests__/index.spec.jsx
+++ src/components/BaggagePicker/components/Option/components/PriorityBoardingInfo/__tests__/index.spec.jsx
@@ -1,6 +1,8 @@
 // @flow strict
 import * as React from "react";
 import { mount } from "enzyme";
+import { ThemeProvider } from "styled-components";
+import defaultTheme from "@kiwicom/orbit-components/lib/defaultTheme";
 
 import PriorityBoardingInfo from "..";
 
@@ -21,11 +23,19 @@ const props = {
 
 describe("#PriorityBoardingInfo", () => {
   test("renders", () => {
-    const wrapper = mount(<PriorityBoardingInfo {...props} />);
+    const wrapper = mount(
+      <ThemeProvider theme={defaultTheme}>
+        <PriorityBoardingInfo {...props} />
+      </ThemeProvider>,
+    );
     expect(wrapper.find("PriorityBoardingInfo").exists()).toBe(true);
   });
   test("renders proper text", () => {
-    const wrapper = mount(<PriorityBoardingInfo {...props} />);
+    const wrapper = mount(
+      <ThemeProvider theme={defaultTheme}>
+        <PriorityBoardingInfo {...props} />
+      </ThemeProvider>,
+    );
     const TextArr = wrapper.find("Text");
     expect(TextArr.first().text()).toBe("baggage_modal.priority_boarding baggage_modal.learn_more");
   });
