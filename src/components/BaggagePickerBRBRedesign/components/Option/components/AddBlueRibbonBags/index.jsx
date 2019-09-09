ba8bc9f561d785a62d67ca182585d6f32dcf1cb8
Robin Cussol <me@robincussol.com>
2019-09-09 15:51:27 +0200

    fix(components): AddBlueRibbonBags fixed to reflect latest redesign

--- src/components/BaggagePickerBRBRedesign/components/Option/components/AddBlueRibbonBags/index.jsx
+++ src/components/BaggagePickerBRBRedesign/components/Option/components/AddBlueRibbonBags/index.jsx
@@ -1,16 +1,20 @@
 // @flow strict
 import * as React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import Text from "@kiwicom/orbit-components/lib/Text";
 import Stack from "@kiwicom/orbit-components/lib/Stack";
 import Button from "@kiwicom/orbit-components/lib/Button";
 import TextLink from "@kiwicom/orbit-components/lib/TextLink";
+import List, { ListItem } from "@kiwicom/orbit-components/lib/List";
 import CheckCircle from "@kiwicom/orbit-components/lib/icons/CheckCircle";
+import Check from "@kiwicom/orbit-components/lib/icons/Check";
+import PlusCircle from "@kiwicom/orbit-components/lib/icons/PlusCircle";
 import Badge from "@kiwicom/orbit-components/lib/Badge";
+import { rtlSpacing } from "@kiwicom/orbit-components/lib/utils/rtl";
 
 import { themeDefault } from "../../../../../../records/Theme";
 import Translate from "../../../../../Translate";
-import TranslateNode from "../../../../../TranslateNode";
+import TranslateRef from "../../../../../TranslateRef";
 import Price from "../../../../../Price";
 import type { PriceType } from "../../../../../../records/Price";
 
@@ -22,27 +26,24 @@ type Props = {|
   openBlueribbonBagsSmartFAQ: () => void,
 |};
 
-const fadeIn = keyframes`
-  from {
-    max-height: 0;
-    opacity: 0;
-  }
-  to {
-    max-height: 100%;
-    opacity: 1;
-  }
+const TransitionWrapper = styled.div`
+  transition: opacity ${themeDefault.orbit.durationNormal} linear;
+  visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
+  opacity: ${({ visible }) => (visible ? "1" : "0")};
+  max-height: ${({ visible }) => (visible ? "285px" : "0")};
 `;
 
-const TranslateNodeWrapper = styled.span`
-  white-space: pre;
+const AddSectionTransitionWrapper = styled(TransitionWrapper)`
+  transition: all ${themeDefault.orbit.durationNormal} linear;
 `;
 
-const RemoveButtonWrapper = styled.div`
-  padding: ${themeDefault.orbit.spaceXXSmall} 0;
+const RemoveSectionTransitionWrapper = styled(TransitionWrapper)``;
+
+const LearnMoreWrapper = styled.div`
+  padding: ${rtlSpacing(`0 0 0 ${themeDefault.orbit.spaceLarge}`)};
 `;
 
 const Wrapper = styled.div`
-  animation: ${fadeIn} ${themeDefault.orbit.durationNormal} ease-in-out;
   overflow: hidden;
 `;
 
@@ -55,55 +56,84 @@ const AddBlueRibbonBags = ({
 }: Props) => {
   return (
     <Wrapper dataTest="BaggagePickerBRBRedesign-AddBlueRibbonBags">
-      <Stack spacing="compact">
-        <Stack spacing="tight">
-          <Text weight="bold" size="small" element="p">
-            <Translate t="baggage_modal.blue_ribbon_bags.lost_bag_protection" />{" "}
+      <Text
+        weight="bold"
+        size="small"
+        element="p"
+        spaceAfter={isBlueRibbonBagAdded ? "small" : "smallest"}
+      >
+        <TranslateRef
+          t="baggage_modal.blue_ribbon_bags.lost_bag_protection"
+          render={refContent => (
             <Text type="secondary" size="small" element="span">
-              <Translate t="baggage_modal.blue_ribbon_bags.provided_by_brb" />
+              {refContent}
             </Text>
-          </Text>
-          <Text element="p" size="small">
-            <Translate t="baggage_modal.blue_ribbon_bags.collect_money_for_missing_bags" />{" "}
-            <TextLink type="secondary" external={false} onClick={openBlueribbonBagsSmartFAQ}>
-              <Translate t="common.learn_more" />
-            </TextLink>
-          </Text>
-        </Stack>
-        {isBlueRibbonBagAdded ? (
-          <RemoveButtonWrapper>
-            <Stack flex align="center">
-              <Badge type="success" icon={<CheckCircle />}>
-                <Translate t="baggage_modal.blue_ribbon_bags.added" />
-              </Badge>
+          )}
+        />
+      </Text>
+      <AddSectionTransitionWrapper visible={!isBlueRibbonBagAdded}>
+        <Stack spacing="compact">
+          <Stack spacing="none">
+            <List size="small">
+              <ListItem icon={<Check />}>
+                <Translate t="baggage_modal.blue_ribbon_bags.track_delayed_bags" />
+              </ListItem>
+              <ListItem icon={<Check />}>
+                <Translate t="baggage_modal.blue_ribbon_bags.collect_money_for_missing_bags" />
+              </ListItem>
+            </List>
+            <LearnMoreWrapper>
               <TextLink
-                type="secondary"
                 size="small"
-                onClick={removeBlueRibbonBag}
-                dataTest="BaggagePickerBRBRedesign-RemoveBlueRibbonBagButton"
+                type="secondary"
+                external={false}
+                onClick={openBlueribbonBagsSmartFAQ}
               >
-                <Translate t="baggage_modal.blue_ribbon_bags.remove" />
+                <Translate t="common.learn_more" />
               </TextLink>
-            </Stack>
-          </RemoveButtonWrapper>
-        ) : (
-          <Button
-            type="primary"
-            size="small"
-            onClick={addBlueRibbonBag}
-            dataTest="BaggagePickerBRBRedesign-AddBlueRibbonBagButton"
+            </LearnMoreWrapper>
+          </Stack>
+          <Stack
+            flex
+            direction="column"
+            spacing="tight"
+            largeMobile={{ direction: "row", spacing: "natural", align: "center" }}
           >
-            <TranslateNodeWrapper>
-              <TranslateNode
-                t="baggage_modal.blue_ribbon_bags.add_bag_protection"
-                values={{
-                  price: <Price value={blueRibbonBagPrice.amount} />,
-                }}
-              />
-            </TranslateNodeWrapper>
-          </Button>
-        )}
-      </Stack>
+            <Stack inline grow={false} align="center">
+              <Button
+                type="primary"
+                size="small"
+                onClick={addBlueRibbonBag}
+                iconLeft={<PlusCircle />}
+                dataTest="BaggagePickerBRBRedesign-AddBlueRibbonBagButton"
+              >
+                <Translate t="baggage_modal.blue_ribbon_bags.add_bag_protection" />
+              </Button>
+              <Text weight="bold" size="small">
+                <Price value={blueRibbonBagPrice.amount} />
+              </Text>
+            </Stack>
+            <Text type="secondary" size="small">
+              <Translate t="baggage_modal.blue_ribbon_bags.applies_all_bags_per_pax" />
+            </Text>
+          </Stack>
+        </Stack>
+      </AddSectionTransitionWrapper>
+      <RemoveSectionTransitionWrapper visible={isBlueRibbonBagAdded}>
+        <Stack flex align="center">
+          <Badge type="success" icon={<CheckCircle />}>
+            <Translate t="baggage_modal.blue_ribbon_bags.added" />
+          </Badge>
+          <TextLink
+            type="secondary"
+            size="small"
+            onClick={removeBlueRibbonBag}
+            dataTest="BaggagePickerBRBRedesign-RemoveBlueRibbonBagButton"
+          >
+            <Translate t="baggage_modal.blue_ribbon_bags.remove" />
+          </TextLink>
+        </Stack>
+      </RemoveSectionTransitionWrapper>
     </Wrapper>
   );
 };
