a5eedd9179645558a940a85165fabea55cffa74d
Petr Snobelt <petr.snobelt@kiwi.com>
2019-10-04 10:50:02 +0200

    feat: Middleware add SKYPICKER_VISITOR_UNIQID to request

--- src/services/middleware/userId.js
+++ src/services/middleware/userId.js
@@ -6,9 +6,17 @@ import { makeUserId } from "../session/ids";
 import { USER_ID } from "../../consts/cookies";
 
 const addCookie = (req: $Request, res: $Response) => {
-  if (req.cookies[USER_ID] && isUUID(req.cookies[USER_ID])) return;
+  if (req.cookies[USER_ID] && isUUID(req.cookies[USER_ID])) {
+    // https://stackoverflow.com/questions/44664555/extending-express-request-class-in-flow
+    // $FlowExpected: assigning request property
+    req[USER_ID] = req.cookies[USER_ID];
+    return;
+  }
 
-  res.cookie(USER_ID, makeUserId());
+  const newId = makeUserId();
+  // $FlowExpected: assigning request property
+  req[USER_ID] = newId;
+  res.cookie(USER_ID, newId);
 };
 
 function userMiddleWare(req: $Request, res: $Response, next: NextFunction) {
