fe385910e25cf51f0ac23532c4b207f964a78590
Bence Olteán <bence.oltean@kiwi.com>
2019-10-23 10:56:28 +0000

    feat(components): BaggageOverview asian names

--- src/components/BaggageOverview/components/BaggageItem/services/getPassengerNames.js
+++ src/components/BaggageOverview/components/BaggageItem/services/getPassengerNames.js
@@ -2,9 +2,16 @@
 
 import type { BaggagePassengerType } from "../../../../../records/Baggage";
 
+const asianCharacters = /[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]/;
+
 const getPassengerNames = (passengersArr: BaggagePassengerType[]): string =>
   passengersArr
-    .map(p => `${p.firstName[0]}. ${p.middleName ? `${p.middleName[0]}. ` : ""}${p.lastName}`)
+    .map(
+      p =>
+        `${asianCharacters.test(p.firstName) ? p.firstName : `${p.firstName[0]}.`} ${
+          p.middleName ? `${p.middleName[0]}. ` : ""
+        }${p.lastName}`,
+    )
     .join(", ");
 
 export default getPassengerNames;
